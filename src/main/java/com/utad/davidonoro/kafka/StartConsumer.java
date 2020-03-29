6f17be3afe3ac9eb295598dc04e191d8ff3fff3d
David O <david.onoro@gmail.com>
2020-03-29 17:15:51 +0200

    Kafka version revisited

--- src/main/java/com/utad/davidonoro/kafka/StartConsumer.java
+++ src/main/java/com/utad/davidonoro/kafka/StartConsumer.java
@@ -2,7 +2,7 @@ package com.utad.davidonoro.kafka;
 
 public class StartConsumer {
 
-    static final String TOPIC = "test";
+    static final String TOPIC = "TOPICO.TEST";
 
     public static void main(String[] args) {
         Consumer consumer = new Consumer(KafkaProperties.getConsumerProperties());
